3a8bc12452dde02c0f4ecffaaf2d347c80c68291
Gess1t <tintamare91@gmail.com>
2026-08-04 19:39:10 +0200

    Add CI builds

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env bash
+
+dotnet restore
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
 dotnet publish src -c Release -o build/plugin/0.6.x
+
+(
+  cd build
+  # zip the DialModeSwitcher.dll
+  jar -cfM DialModeSwitcher.zip ./*.dll
+
+  sha256sum DialModeSwitcher.zip >> hashes.txt
+)
